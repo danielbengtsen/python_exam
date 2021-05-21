991e9bc9bdad1683cfe90c18c66775762759c4ab
UtilExe <laymasterlolz@gmail.com>
2021-05-21 10:21:08 +0200

    Fix clustering, move clustering into a separate file, and clean up first cluster that uses several features in its dataset.

---
+++ modules/clustering_insert.py
@@ -0,0 +1,39 @@
+from sklearn import preprocessing
+from sklearn.cluster import MeanShift, estimate_bandwidth
+import pandas as pd
+import numpy as np 
+
+# Normalization
+def doNormalization(meanshift_except_major):
+    scaler = preprocessing.MinMaxScaler()
+    names = meanshift_except_major.columns
+    d = scaler.fit_transform(meanshift_except_major)
+    return pd.DataFrame(d, columns=names)
+
+#We will add a new column in dataset which shows the cluster the data of a particular row belongs to.
+def addClusterGroupToDataSet(meanshift_data_scaled, labels):
+    meanshift_data_scaled['cluster_group'] = np.nan
+    data_length=len(meanshift_data_scaled)
+    for i in range(data_length):
+        meanshift_data_scaled.iloc[i,meanshift_data_scaled.columns.get_loc('cluster_group')] = labels[i]
+    return meanshift_data_scaled
+
+def makeAnalyzer(meanshift_data_scaled):
+    #We found the bandwith using the estimate_bandiwth function mentioned in below cell.
+    analyzer = MeanShift(bandwidth=1.45) #We will provide only bandwith in hyperparameter . The smaller values of bandwith result in tall
+    #skinny kernels & larger values result in short fat kernels.
+    analyzer.fit(meanshift_data_scaled)
+    return analyzer
+    
+
+def makeLabelsFromAnalyzer(analyzer):
+    labels = analyzer.labels_
+    return labels
+
+def calculateAmountOfClusters(analyzer):
+    labels_data = analyzer.labels_
+    cluster_centers = analyzer.cluster_centers_
+
+    labels_unique = np.unique(labels_data)
+    n_clusters = len(labels_unique)
+    print('Number of estimated clusters : {}'.format(n_clusters))
