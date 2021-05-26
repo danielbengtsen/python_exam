330d3248b433f6cce94d3a5504887a5ab2302c41
Bloom9900 <jannichhoejmose@gmail.com>
2021-05-26 21:26:50 +0200

    Prediction progress

--- modules/prediction_insert.py
+++ modules/prediction_insert.py
@@ -1,6 +1,7 @@
 import sklearn
 import sklearn.linear_model
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
 
 # consider making it a class with an object, to retrieve variables better, etc. (like with the ~~ week3/week4 project)
 
@@ -8,12 +9,12 @@ import numpy as np
 def excludeYear(data_for_prediction, yearToExclude):
     return (data_for_prediction['Year'] != yearToExclude)
 
-def shouldExcludeYears(yes, data_for_prediction):
+def shouldExcludeYears(yes, data_for_prediction, industry):
     if (yes):
-        data_crisis_years_excluded = data_for_prediction[(data_for_prediction["Education.Major"] == 'Computer Science and Math') & excludeYear(data_for_prediction, 2001) & excludeYear(data_for_prediction, 2003) & excludeYear(data_for_prediction, 2006) & excludeYear(data_for_prediction, 2008)]
+        data_crisis_years_excluded = data_for_prediction[(data_for_prediction["Education.Major"] == industry) & excludeYear(data_for_prediction, 2001) & excludeYear(data_for_prediction, 2003) & excludeYear(data_for_prediction, 2006) & excludeYear(data_for_prediction, 2008)]
         return data_crisis_years_excluded
     else:
-        data_crisis_years_included = data_for_prediction[(data_for_prediction["Education.Major"] == 'Computer Science and Math')]
+        data_crisis_years_included = data_for_prediction[(data_for_prediction["Education.Major"] == industry)]
         return data_crisis_years_included
 
 def get_xs_reshape(dataset_for_prediction, column_name):
@@ -32,8 +33,10 @@ def createLinearReg(dataset_for_prediction, xs_reshape, ys, column_name):
     # Reshape data for X
     model = createModel(xs_reshape, ys)
     model.coef_
-    data_set_new.plot(x=0,y=1) # x= first column and y=second column in dataframe
-    #data.plot.scatter(x = 1, y = 3)
+    #data_set_new.plot(x=0,y=1) # x= first column and y=second column in dataframe
+    data_set_new.plot.scatter(x = 0, y = 1)
+    plt.plot([data_set_new['Demographics.Total'].min(), data_set_new['Demographics.Total'].max()], [data_set_new['Salaries.Mean'].min(), data_set_new['Salaries.Mean'].max()])
+    #
     
 def createModel(xs_reshape, ys):
     model = sklearn.linear_model.LinearRegression()
